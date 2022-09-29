a91f50102a28d3e6653cff4c3153f9b3597bbdb7
Sai <r4ksai@gmail.com>
2022-09-30 03:28:58 +0530

    ☘️ Add reset and eeprom config to the device

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,42 @@
 #include <EEPROM.h>
 #include "Display/display.h"
 
+#define RESET_PIN 4
+const int SHORT_PRESS_TIME = 1000;
+int lastState = LOW;
+int currentState;
+unsigned long pressedTime = 0;
+unsigned long realeasedTime = 0;
+
 const char* ap_ssid = "Git Status Tracker";
 const char* ap_password = "123456789";
 
 WebServer server(80);
 RawDisplay rawDisplay = RawDisplay();
+
+void reset() {
+  EEPROM.begin(512);
+  for (int i = 0; i < 512; i++) {
+    EEPROM.write(i, 0);
+  }
+  EEPROM.commit();
+  EEPROM.end();
+
+  ESP.restart();
+}
+
+void saveCreds(String ssid, String password) {
+  char buff1[30];
+  char buff2[30];
+  ssid.toCharArray(buff1, 30);
+  password.toCharArray(buff2, 30);
+
+  EEPROM.begin(400);
+    EEPROM.writeString(100, buff1);
+    EEPROM.writeString(200, buff2);
+    EEPROM.commit();
+  EEPROM.end();
+}
 
 void handleNotFound(){
   server.send(404, "text/plain", "Not found");
@@ -23,26 +54,35 @@ void handleHome() {
         <style>
           html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}
           body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}
-          p {font-size: 14px;color: #888;margin-bottom: 10px;}
+          form{display: flex; flex-direction: column;}
+          input{display: block; padding: 20px 10px;}
         </style>
       </head>
       <body>
         <h1>Git Status Tracker</h1>
         <h3>Configure the Device</h3>
         <form>
-          <input placeholder="SSID" />
-          <input placeholder="Password" />
-          <input type="submit" value="Submit" />
+          <input placeholder="SSID" name="ssid" maxlength="30" />
+          <input placeholder="Password" name="password" maxlength="30" />
+          <input type="submit" value="Save" />
         </form>
       </body>
   </html>
   )rawliteral";
 
-  server.send(200, "text/html", homePage);
+  if (server.hasArg("ssid") && server.hasArg("password")) {
+    String ssid = server.arg("ssid");
+    String password = server.arg("password");
+    saveCreds(ssid, password);
+    ESP.restart();
+  }
+  else {
+    server.send(200, "text/html", homePage);
+  }
+
 }
 
 void accessPoint() {
-    WiFi.mode(WIFI_AP);
     WiFi.softAP(ap_ssid, ap_password);
     delay(100);
     IPAddress IP(192, 168, 0, 1);
@@ -51,7 +91,6 @@ void accessPoint() {
 }
 
 void connectToWifi(char* ssid, char* password) {
-  // TODO: Check if credentials exists if not then go with accesspoint or else try wifi
     int statusChecks = 0;
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
@@ -64,20 +103,41 @@ void connectToWifi(char* ssid, char* password) {
     }
 }
 
+void readCreds() {
+  EEPROM.begin(400);
+    String ssid = EEPROM.readString(100);
+    String password = EEPROM.readString(200);
+  EEPROM.end();
+
+  char ssidBuffer[30];
+  char passwordBuffer[30];
+
+  ssid.toCharArray(ssidBuffer, 30);
+  password.toCharArray(passwordBuffer, 30);
+
+  if (ssid.length() <= 0 && password.length() <= 0) 
+    accessPoint();
+  else 
+    connectToWifi(ssidBuffer, passwordBuffer);
+
+  Serial.print("SSID : ");
+  Serial.println(ssid);
+  Serial.print("Password : ");
+  Serial.println(password);
+}
+
 void setup(void)
 {
 
-  // EEPROM.begin(4);
-  // uint next = EEPROM.readUInt(0);
+  // Set RESET PIN
+  pinMode(RESET_PIN, INPUT_PULLUP);
 
-  // String ssid = EEPROM.readString(2);
-  // String password = EEPROM.readString(next);
+  Serial.begin(9600);
+  readCreds();
 
-  // EEPROM.end();
-
-  // server.on("/", handleHome);
-  // server.onNotFound(handleNotFound);
-  // server.begin();
+  server.on("/", handleHome);
+  server.onNotFound(handleNotFound);
+  server.begin();
 
   rawDisplay.begin();
 }
@@ -91,7 +151,22 @@ byte status[] = {
 
 void loop(void)
 {
-  // server.handleClient();
+  currentState = digitalRead(RESET_PIN);
+
+  if (lastState == HIGH && currentState == LOW)
+    pressedTime = millis();
+  else if (lastState == LOW && currentState == HIGH) {
+    realeasedTime = millis();
+
+    long pressDuration = realeasedTime - pressedTime;
+
+    if (pressDuration < SHORT_PRESS_TIME)
+      reset();
+
+    lastState = currentState;
+
+  }
+  server.handleClient();
   // rawDisplay.loading();
   rawDisplay.gitStatus(7, 0, status);
 }
